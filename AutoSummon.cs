a16916e53cc10dcad91dc1e127927989559c0b02
Andenis <andenisbu@gmail.com>
2024-07-11 22:43:21 +0300

    Finished working on 1.2

--- AutoSummon.cs
+++ AutoSummon.cs
@@ -10,7 +10,10 @@ namespace LPAutoSummon
         private int  previousMinionSlotIndex;   // Store the index of inventory slot for minions
         private int  previousMinionCap;         // Store the previous max minions count
         private Item previousMinionSlotItem;    // Store the previous item in the inventory slot for minions
+
         private bool hasSpawned;                // Flag to check if the player has respawned
+        private bool isUsingMinionSlotItem;     // Flag to check if is using an item
+        private bool hasUsedMinionSlotItem;     // Flag to check if user has used an item
 
         // Initialize the mod player
         public override void Initialize()
@@ -22,28 +25,69 @@ namespace LPAutoSummon
             previousMinionCap = 0;
      
             hasSpawned = false;
+            isUsingMinionSlotItem = false;
+            hasUsedMinionSlotItem = false;
         }
 
-        // Helper method to get the index of inventory slot for minions
+        // Get the index of inventory slot for minions
         private int GetConfigMinionSlotIndex()
         {
             return ModContent.GetInstance<ModConfigs>().MinionSlot - 1;
+        }
+
+        // Get the total number of used up minion slot
+        private float GetMinionSlotsUsed()
+        {
+            float minionSlotsUsed = 0f;
+            foreach (Projectile proj in Main.projectile)
+            {
+                if (proj.active && proj.owner == Main.myPlayer && proj.minion)
+                {
+                    minionSlotsUsed += proj.minionSlots;
+                }
+            }
+
+            return minionSlotsUsed;
+        }
+
+        // Remove all the minions that the palyer has summoned
+        private void removeAllSummonMinions()
+        {
+            foreach (Projectile proj in Main.projectile)
+            {
+                if (proj.active && proj.owner == Main.myPlayer && proj.minion)
+                {
+                    proj.Kill();
+                }
+            }
+        }
+
+        // Return true if the item is a summoner and not a sentry
+        private bool checkItemSummonsMinions(Item item)
+        {
+            return item.DamageType == DamageClass.Summon && !item.sentry ? true : false;
         }
 
         // Automatically summon minions
         private void SummonMinions()
         {
             // Check if the item in the configured slot is a valid summon weapon
-            if (Player.inventory[previousMinionSlotIndex].DamageType != DamageClass.Summon || Player.inventory[previousMinionSlotIndex].sentry)
+            if (!checkItemSummonsMinions(Player.inventory[previousMinionSlotIndex]))
             {
-                return; // Exit if the item is not a valid summon weapon
-            }
+                return; 
+            }      
 
+            // Select configured minion slot item
             int previousSelectedItem = Player.selectedItem;
             Player.selectedItem = previousMinionSlotIndex;
 
+            
+            Projectile proj = new Projectile();
+            proj.SetDefaults(Player.HeldItem.shoot);
+
+            float minionSlotsUsed = GetMinionSlotsUsed();
             // Loop through the max minions count to summon them
-            for (int i = 0; i < Player.maxMinions; ++i)
+            while (minionSlotsUsed + proj.minionSlots <= Player.maxMinions)
             {
                 float previousScreenPositionX = Main.screenPosition.X;
                 float previousScreenPositionY = Main.screenPosition.Y;
@@ -70,6 +114,8 @@ namespace LPAutoSummon
 
                 Main.screenPosition.X = previousScreenPositionX; // Restore screen position X
                 Main.screenPosition.Y = previousScreenPositionY; // Restore screen position Y
+
+                minionSlotsUsed += proj.minionSlots;
             }
 
             Player.selectedItem = previousSelectedItem;
@@ -82,27 +128,80 @@ namespace LPAutoSummon
             hasSpawned = true;
         }
 
-        // Update method called every frame
-        public override void PostUpdate()
+        public override void PreUpdate()
         {
-            base.PostUpdate();
+            base.PreUpdate();
+            // Check if the game is running on a dedicated server or the player instance is not the current player,
             if (Main.dedServ || Player.whoAmI != Main.myPlayer)
             {
                 return;
             }
 
+            // Remove all currently summoned minions if conditions are met
+            if (previousMinionSlotItem != Player.inventory[previousMinionSlotIndex])
+            {
+                removeAllSummonMinions(); 
+            }
+
+
+            // Activate flag to check if conditions are met
+            if (Player.selectedItem == GetConfigMinionSlotIndex() && // Check if the select item is configured minion slot item
+                checkItemSummonsMinions(Player.HeldItem) && // Check if the held item is a minion summon item
+                Player.controlUseItem) // Check if the player is using an item
+            {
+                if (!isUsingMinionSlotItem)
+                {
+                    isUsingMinionSlotItem = true;
+                    hasUsedMinionSlotItem = true;
+
+
+                    int previousMana = Player.HeldItem.mana;
+                    Player.HeldItem.mana = 0; // Prevent mana consumption
+
+                    // Trigger item use
+                    Player.controlUseItem = true;
+                    Player.ItemCheck();
+                    Player.controlUseItem = false;
+
+                    Player.HeldItem.mana = previousMana; // Restore previous mana cost
+
+                    // Wait until the item animation is finished
+                    while (Player.itemAnimation > 0)
+                    {
+                        Player.ItemCheck();
+                    }
+                }
+            }
+            else
+            {
+                isUsingMinionSlotItem = false;
+            }
+        }
+
+        public override void PostUpdate()
+        {
+            base.PostUpdate();
+            // Check if the game is running on a dedicated server or the player instance is not the current player,
+            if (Main.dedServ || Player.whoAmI != Main.myPlayer)
+            {
+                return;
+            }
+
+            // Summon minions if conditions are met
             if (previousMinionCap != Player.maxMinions || // Check if the max minions count has changed
                 previousMinionSlotItem != Player.inventory[previousMinionSlotIndex] || // Check if the item in the configured minion slot has changed
+                hasUsedMinionSlotItem || // Check if the item in the configured minion slot has been used
                 hasSpawned) // Check if if the player has respawned
             {
-                SummonMinions(); // Summon minions if conditions are met
+                SummonMinions(); 
                 hasSpawned = false;
+                hasUsedMinionSlotItem = false;
             }
 
             // Update stored values
             previousMinionCap = Player.maxMinions;           
             previousMinionSlotItem = Player.inventory[previousMinionSlotIndex];
-            previousMinionSlotIndex = GetConfigMinionSlotIndex();
+            previousMinionSlotIndex = GetConfigMinionSlotIndex(); 
         }
     }
 }
--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # LP's Auto Summon
 **LP's Auto Summon** is a mod for Terraria that enhances the summoner gameplay experience by automating the summoning of minions. With this mod, you no longer need to manually summon each minion one by one. Instead, just place your summoner weapon in the designated hotbar slot and the mod ensures that your maximum number of minions are automatically summoned. The mod also automatically re-summons your minions after you respawn.
 
-## What's New in 1.1
-- **Configurable Hotbar Slot:** You can now configure which hotbar slot to use for summoning, with the 10th slot still being the default.
-- **Bug Fix:** Fixed a minor bug where summoning sounds were played for everyone in multiplayer when another player triggered their auto summoning.
+## What's New in 1.2
+- **Support for Minions Using Less Than 1 Slot:** Added support for summoning minions who take up less space than 1 slot.
+- **Auto Summon on Summon Item Use:** Trigger auto summon when you use your designated summoning slot item to fill in the slots.
+- **Re-summon on Item Swap:** Swapping your summoning item in the selected summon slot will now re-summon everything as the new summoner item.
+
 
 ## How It Works
-- **Hotbar Slot Selection:** The mod monitors the item in your chosen hotbar slot. If it is a summoning weapon (and not a sentry), it will use this item for auto-summoning.
+- **Summoning Weapon Detection:** The mod constantly monitors the item in your selected hotbar slot. If the item is a summoning weapon (excluding sentries), it will automatically use this item for summoning minions.
 - **Adjusts to Minion Capacity:** Whenever your maximum minion capacity changes (e.g., equipping new gear or buffs), the mod will automatically summon the appropriate number of minions.
-- **Player Respawn:** After respawning, the mod will ensure your minions are summoned back automatically.
+- **Re-summon on Player Respawn:** After respawning, the mod will ensure your minions are summoned back automatically.
+- **Configurable Hotbar Slot:** You can configure which hotbar slot to use for summoning, with the 10th slot being the default.
 
 ## Compatibility
 LP's Auto Summon is compatible with most other mods. If you encounter any issues, please report them on the [GitHub issues page](https://github.com/Andenis-Bu/LPAutoSummon/issues).
